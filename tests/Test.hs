99306a87d9c9555466ceb394c90fc7d351d462d6
Oliver Lea <olilea3@gmail.com>
2017-01-31 18:26:21 +0000

    Finally got Tasty tests up and running successfully

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,9 +1,19 @@
 
-module Main where
+import Test.Tasty
+import Test.Tasty.HUnit
 
-import System.Exit (exitFailure)
+import Data.List
+import Data.Ord
 
 main :: IO ()
-main = do
-    putStrLn "This test always fails"
-    exitFailure
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [unitTests]
+
+unitTests = testGroup "Unit tests"
+    [ testCase "List comparison (different length)" $
+        ([1, 2, 3] `compare` [1, 2]) @?= GT
+    , testCase "List comparison (same length)" $
+        ([1, 2, 3] `compare` [1, 2, 2]) @?= LT
+    ]
