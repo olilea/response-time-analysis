fab5a70fc7a205404fb6955c6831e661acb3343f
Oliver Lea <olilea3@gmail.com>
2017-02-09 11:04:03 +0000

    Work on the GA...

--- src/evolution/Main.hs
+++ src/evolution/Main.hs
@@ -13,13 +13,15 @@ import Data.Ord
 import qualified Data.Map as M
 
 import System.Random
+import System.Random.Shuffle
 
--- Need a way to assign fitness to each thing
 
--- Maps priority to task ID
+-- Maps task ID to priority
 type Priorities = [(Int, Int)]
 -- Maps task ID to core ID
 type Mapping = [(Int, Int)]
+
+data Domain = Domain [Core] [Task] Platform
 
 data EvolutionParameters = EvolutionParameters {
     eGenerations :: Int,
@@ -27,6 +29,24 @@ data EvolutionParameters = EvolutionParameters {
     eCrossoverRate :: Float,
     eMutationRate :: Float
 }
+
+-- Mutation
+
+swapMutate :: (MonadRandom m, Eq a)
+           => Float
+           -> [a]
+           -> m [a]
+swapMutate mtPb mp = do
+  rs <- take 2 <$> getRandomRs (0, (length mp) - 1)
+  let mapping1 = mp !! (rs !! 0)
+  let mapping2 = mp !! (rs !! 1)
+  let swap x
+        | x == mapping1 = mapping2
+        | x == mapping2 = mapping1
+        | otherwise = x
+  return $ []
+
+-- Crossover
 
 singlePointCrossover :: (MonadRandom m)
                      => [a]
@@ -36,14 +56,22 @@ singlePointCrossover l r = do
     cxPoint <- getRandomR (0, length l)
     return $ (take cxPoint l) ++ (drop cxPoint r)
 
--- mappingcrossover :: (RandomGen g)
---                  => g 
---                  -> TaskMapping
---                  -> TaskMapping
---                  -> Rand g TaskMapping
--- mappingCrossover g tmLeft tmRight = (M.fromList offspring, g') = do
---     let order = sortBy (comparing fst) . M.toList
---     let (sl, sr) = (order tmLeft, order tmRight)
+mappingCrossover :: (MonadRandom m)
+                 => Mapping
+                 -> Mapping
+                 -> m Mapping
+mappingCrossover = singlePointCrossover
+
+-- How should this even be implemented?
+priorityCrossover :: (MonadRandom m)
+                  => Priorities
+                  -> Priorities
+                  -> m Priorities
+priorityCrossover l r = singlePointCrossover (order l) (order r)
+  where
+    order = sortBy (comparing fst)
+
+-- Generating the original population
 
 genMapping :: (MonadRandom m) => Int -> Int -> m Mapping
 genMapping ts cs = mapM prod [1..ts]
@@ -53,40 +81,69 @@ genMapping ts cs = mapM prod [1..ts]
             return (i, r)
 
 genPriorities :: (MonadRandom m) => Int -> m Priorities
-genPriorities ts = mapM prod priorities
-    where
-        priorities = [1..ts]
-        prod i = do 
-            r <- getRandomR (1, ts)
-            return (i, r)
+genPriorities ts = do 
+  priorities <- shuffleM [1..ts]
+  let tasks = [1..ts]
+  return $ zip tasks priorities
 
 genPopulations :: (MonadRandom m)
                => Int 
                -> Int
                -> Int 
                -> m ([Priorities], [Mapping])
-genPopulations popSize ts cs = do
+genPopulations popSize cs ts = do
     let populate = replicateM popSize
     ps <- populate $ genPriorities ts
     ms <- populate $ genMapping ts cs
     return (ps, ms)
 
-coevolve :: (RandomGen g)
+-- Actual GA implementation
+
+runGA :: (RandomGen g)
          => g
          -> EvolutionParameters
-         -> [Core]
-         -> [Task]
-         -> Platform
-         -> ([Priorities], [Mapping])
-coevolve g ep cs ts p = evalRand evolve g
+         -> Domain
+         -> (Domain -> Priorities -> Mapping -> Float)
+         -> (Priorities, Mapping)
+runGA g ep d@(Domain cs ts p) fnf = evalRand run g
+  where
+    run = do
+        pop <- zeroGen ep d fnf
+        runGA' ep d fnf pop 0
+
+-- Create an initial population and assign them each a fitness.
+-- This fitness is the result of a random pairing across the
+-- subpopulations.
+zeroGen ep d@(Domain cs ts _) fnf = do
+  (ps, ms) <- genPopulations popSize cs ts
     where
-        evolve = genPopulations (ePopulationSize ep) (length ts) (length cs)
+      popSize = ePopulationSize ep cs ts
 
-cores = [Core idee 1.0 | idee <- [1..9]]
+runGA' :: (MonadRandom m)
+          => EvolutionParameters
+          -> Domain
+          -> (Domain -> Priorities -> Mapping -> Float)
+          -> ([Priorities], [Mapping])
+          -> Int
+          -> m (Priorities, Mapping)
+runGA' ep@(EvolutionParameters gens _ cxPb mtPb) d fnf pop@(ps, ms) genNumber
+  | genNumber == gens = return (head ps, head ms)
+  | otherwise = do
+      pop' <- evolve ep d fnf pop
+      runGA' ep d fnf pop (genNumber + 1)
 
+-- Evolve a population into the next generation using the provided
+-- fitness function and parameters
+evolve :: (MonadRandom m)
+       => EvolutionParameters
+       -> Domain
+       -> (Domain -> Priorities -> Mapping -> Float)
+       -> ([(Priorities, Float)], ([(Mapping, Float)]))
+evolve ep@(EvolutionParameters _ _ cxPb mtPb) d fnf pop@(ps, ms) = return pop
+  
 
 main :: IO ()
-main = putStrLn $ show $ coevolve g ep cs ts p
+main = putStrLn $ show $ runGA g ep (Domain cs ts p)
     where
         g = mkStdGen 42
         ep = EvolutionParameters 10 10 0.5 0.5
