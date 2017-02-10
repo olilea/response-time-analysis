4558ec86f513bbe6a5f9f1d6a6d531727a6936a3
Oliver Lea <olilea3@gmail.com>
2017-02-10 22:25:26 +0000

    Reorganized folder structure

--- src/lib/Coevolve.hs
+++ src/lib/Coevolve.hs
@@ -1,5 +1,8 @@
 
-module Main
+module Coevolve
+  ( runGA
+  , EvolutionParameters(..)
+  , Domain(..))
 where
 
 import Analysis
@@ -316,42 +319,3 @@ evaluateFitness fnf (pRep, mRep) (ps, ts) = (pFit, tFit)
     pFit = map (\p -> (p, fnf p mRep)) ps
     tFit = map (\m -> (m, fnf pRep m)) ts
 
-gimme :: [Task]
-gimme = [Task idee 20.0 20.0 1.0 (Communication (destination idee) 5)
-            | idee <- [1..6]]
-            where
-                destination tIdee = fromJust $ M.lookup tIdee destLookup
-                destLookup = M.fromList
-                    [ (1, 4)
-                    , (2, 4)
-                    , (3, 1)
-                    , (4, 3)
-                    , (5, 2)
-                    , (6, 2)
-                    ]
-
-main :: IO ()
-main = do
-  let mappings = runGA g ep d fnf
-  putStrLn . show $ mappings
-  putStrLn . show $ fnf d (fst mappings) (snd mappings)
-    where
-        g = mkStdGen 44535432443
-        ep = EvolutionParameters 200 100 5 60 0.5 0.05
-        cs = [Core idee 1.0 | idee <- [1..9]]
-        ts = [Task idee 20.0 20.0 1.0 (Communication (destination idee) 5)
-            | idee <- [1..6]]
-            where
-                destination tIdee = fromJust $ M.lookup tIdee destLookup
-                destLookup = M.fromList
-                    [ (1, 4)
-                    , (2, 4)
-                    , (3, 1)
-                    , (4, 3)
-                    , (5, 2)
-                    , (6, 2)
-                    ]
-        coreMapping = M.fromList $ zip [1..9] [Location r c | r <- [1..3], c <- [1..3]]
-        p = Platform 1.0 1.0 1.0
-        d = Domain cs ts p
-        fnf d pm tm = fromIntegral $ missingDeadlines p 7.0 (Application cs ts (M.fromList tm) coreMapping (M.fromList pm))
---
+++ src/solver/Main.hs
@@ -0,0 +1,39 @@
+
+module Main where
+
+import qualified Data.Map as M
+import Data.Maybe
+
+import System.Random
+
+import Analysis
+import Coevolve
+
+main :: IO ()
+main = do
+  let mappings = runGA g ep d fnf
+  putStrLn . show $ mappings
+  putStrLn . show $ fnf d (fst mappings) (snd mappings)
+  putStrLn . show $ end d (fst mappings) (snd mappings)
+    where
+        g = mkStdGen 44535432443
+        ep = EvolutionParameters 200 100 5 60 0.5 0.05
+        cs = [Core idee 1.0 | idee <- [1..9]]
+        ts = [Task idee 20.0 20.0 1.0 (Communication (destination idee) 5)
+            | idee <- [1..6]]
+            where
+                destination tIdee = fromJust $ M.lookup tIdee destLookup
+                destLookup = M.fromList
+                    [ (1, 4)
+                    , (2, 4)
+                    , (3, 1)
+                    , (4, 3)
+                    , (5, 2)
+                    , (6, 2)
+                    ]
+        coreMapping = M.fromList $ zip [1..9] [Location r c | r <- [1..3], c <- [1..3]]
+        p = Platform 1.0 1.0 1.0
+        d = Domain cs ts p
+        fnf d pm tm = fromIntegral $ missingDeadlines p 3.0 (Application cs ts (M.fromList tm) coreMapping (M.fromList pm))
+
+        end d pm tm = endToEnd p 3.0 (Application cs ts (M.fromList tm) coreMapping (M.fromList pm))
